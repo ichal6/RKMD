6d0b50a6540f802633c1cf799d7e45b09b1f44be
Rafal Pajdak <rpajdak@gmail.com>
2020-04-08 23:56:59 +0200

    Add ProductDBDAO class.

--- src/main/java/DAO/ProductDBDAO.java
+++ src/main/java/DAO/ProductDBDAO.java
@@ -1,4 +1,41 @@
 package DAO;
 
-public class ProductDBDAO implements ProductDAO {
+import Model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDBDAO implements ProductDAOAdmin {
+
+    private List<Product> productsList = new ArrayList<>();
+
+    @Override
+    public List<Product> getAllProducts() {
+        return null;
+    }
+
+    @Override
+    public List<Product> searchProducts() {
+        return null;
+    }
+
+    @Override
+    public void decreaseQuantity(Product product) {
+
+    }
+
+    @Override
+    public void addProductToInventory(Product product) {
+
+    }
+
+    @Override
+    public void deleteProduct(Product product) {
+
+    }
+
+    @Override
+    public void updateInventory(Product product) {
+
+    }
 }
