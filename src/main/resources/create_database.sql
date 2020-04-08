ff82176bd7938f747d8ca2cb2ac1957e5aa95922
Dariusz Raba <rabadariusz@gmail.com>
2020-04-08 21:42:38 +0200

    Update create_database.sql file

--- src/main/resources/create_database.sql
+++ src/main/resources/create_database.sql
@@ -1,3 +1,14 @@
+DROP TABLE IF EXISTS Bike_product CASCADE;
+DROP TABLE IF EXISTS Order_table CASCADE;
+DROP TABLE IF EXISTS Order_product CASCADE;
+DROP TABLE IF EXISTS User_table CASCADE;
+DROP TABLE IF EXISTS AccountDetails CASCADE;
+DROP TABLE IF EXISTS Customer_order CASCADE;
+
+
+
+
+
 CREATE TABLE Bike_product(
    Product_ID serial PRIMARY KEY,
 	Name varchar(25) NOT NULL,
@@ -33,5 +44,50 @@ CONSTRAINT Order_product_Bike_product_ID_fkey FOREIGN KEY (Bike_product_ID)
 	
 	
 );
+
+
+
+
+
+
+CREATE TABLE AccountDetails(
+	AccountDetails_ID serial PRIMARY KEY,
+	Date_of_creation DATE NOT NULL,
+	Password VARCHAR(25) not null,
+	Login varchar(25) not null
+);
+
+
+CREATE TABLE User_table (
+   	User_ID serial unique,
+	First_name varchar(25) NOT NULL,
+	Last_name varchar(25) NOT NULL,
+	Password varchar(25) NOT NULL,
+	Login varchar(25) NOT NULL,
+	Admin_user BIT NOT NULL,
+	Account_Details_ID serial,
+PRIMARY KEY (User_ID, Account_Details_ID),
+CONSTRAINT User_table_Account_Details_ID_fkey FOREIGN KEY (Account_Details_ID)
+      REFERENCES AccountDetails (AccountDetails_ID) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+
+);
+
+
+CREATE TABLE Customer_order(
+	Customer_order_ID serial,
+	User_ID serial,
+	Order_ID serial,
+	PRIMARY KEY (Customer_order_ID, User_ID, Order_ID),
+CONSTRAINT Customer_order_User_ID_fkey FOREIGN KEY (User_ID)
+      REFERENCES User_table (User_ID) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+	
+CONSTRAINT Customer_order_Order_ID_fkey FOREIGN KEY (Order_ID)
+      REFERENCES Order_Table (Order_ID) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+
 
 
