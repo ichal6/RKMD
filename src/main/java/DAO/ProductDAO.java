297bbae7c29400562de1a9827d0008ecf37c738f
Rafal Pajdak <rpajdak@gmail.com>
2020-04-08 23:56:25 +0200

    Add ProductDAO class.

--- src/main/java/DAO/ProductDAO.java
+++ src/main/java/DAO/ProductDAO.java
@@ -1,4 +1,13 @@
 package DAO;
 
+import Model.Product;
+
+import java.util.List;
+
 public interface ProductDAO {
+    public List<Product> getAllProducts();
+
+    public List<Product> searchProducts();
+
+    public void decreaseQuantity(Product product);
 }
