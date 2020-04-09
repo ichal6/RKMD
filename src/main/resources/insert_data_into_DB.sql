f7dd2d22787facb40112429f767eb006290ea394
Rafal Pajdak <rpajdak@gmail.com>
2020-04-09 21:24:23 +0200

    Add 4 bikes to db.

--- src/main/resources/insert_data_into_DB.sql
+++ src/main/resources/insert_data_into_DB.sql
@@ -1,2 +1,4 @@
-INSERT INTO bike_product VAlUES (DEFAULT,'Cross',200,'Blue','M',2);
-INSERT INTO bike_product VAlUES (DEFAULT,'Crosss',200,'Blue','M',2);
+INSERT INTO bike_product VAlUES (DEFAULT,'Kross',1999,'Blue','M',2);
+INSERT INTO bike_product VAlUES (DEFAULT,'Romet',1499,'Blue','M',2);
+INSERT INTO bike_product VAlUES (DEFAULT,'Wigry',1499,'Blue','M',2);
+INSERT INTO bike_product VAlUES (DEFAULT,'BMX',1499,'Blue','M',2);
