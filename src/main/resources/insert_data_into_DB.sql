d2b37c20b503296a574b4149fb77ac7fb102868d
Dariusz Raba <rabadariusz@gmail.com>
2020-04-10 08:39:06 +0200

    Insert Admins into DB

--- src/main/resources/insert_data_into_DB.sql
+++ src/main/resources/insert_data_into_DB.sql
@@ -2,3 +2,16 @@ INSERT INTO bike_product VAlUES (DEFAULT,'Kross',1999,'Blue','M',2);
 INSERT INTO bike_product VAlUES (DEFAULT,'Romet',1499,'Blue','M',2);
 INSERT INTO bike_product VAlUES (DEFAULT,'Wigry',1499,'Blue','M',2);
 INSERT INTO bike_product VAlUES (DEFAULT,'BMX',1499,'Blue','M',2);
+
+
+INSERT INTO accountdetails VALUES (DEFAULT, '2020-04-10','goracykonrad','konradpass1');
+INSERT INTO User_table VALUES (DEFAULT, 'Konrad', 'Siekacz', 'konradpass1','goracykonrad', '1', DEFAULT);
+
+INSERT INTO accountdetails VALUES (DEFAULT, '2020-04-10','goracyrafal','rafalpass1');
+INSERT INTO User_table VALUES (DEFAULT, 'Rafal', 'Pajdak', 'rafalpass1','goracyrafal', '1', DEFAULT);
+
+INSERT INTO accountdetails VALUES (DEFAULT, '2020-04-10','goracymichal','michalpass1');
+INSERT INTO User_table VALUES (DEFAULT, 'Michal', 'Lechowicz', 'michalpass1','goracymichal', '1', DEFAULT);
+
+INSERT INTO accountdetails VALUES (DEFAULT, '2020-04-10','goracydariusz','dariuszpass1');
+INSERT INTO User_table VALUES (DEFAULT, 'Dariusz', 'Raba', 'dariuszpass1','goracydariusz', '1', DEFAULT);
