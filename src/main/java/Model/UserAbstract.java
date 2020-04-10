a6ff751df2c37d3aa7ea2ccbd178f495f087d676
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-04-10 12:57:25 +0200

    Add fields to UserAbstract

--- src/main/java/Model/UserAbstract.java
+++ src/main/java/Model/UserAbstract.java
@@ -1,4 +1,9 @@
 package Model;
 
 public abstract class UserAbstract {
+    protected String ID;
+    protected String name;
+    protected String surname;
+    protected String login;
+    protected String password;
 }
