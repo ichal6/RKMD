93d7fcd1e5c8d2d8a67f5594911b873524bd3fcc
Dariusz Raba <rabadariusz@gmail.com>
2020-04-10 20:26:37 +0200

    Clean Main and Admin class before merge to avoid conflicts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,5 @@
-import DAO.AdminDatabaseDAO;
-import Model.Admin;
-import org.w3c.dom.ls.LSOutput;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        AdminDatabaseDAO db = new AdminDatabaseDAO();
-//        db.getAllAdmins();
-//        List<Admin> adminslist = db.getAdminList();
-//        for (Admin admin:adminslist
-//        ) {
-//            System.out.println(admin.toString());
-//
-//        }
-        String[] newAdmin = new String[5];
-        newAdmin[0] = "Natalka";
-        newAdmin[1] = "Raba";
-        newAdmin[2] = "kaya";
-        newAdmin[3] = "gogogo";
-        newAdmin[4] = "1";
-
-//        db.addAdmin(newAdmin);
-            db.deleteAdmin(5);
-//        db.updateAdmin(5,newAdmin);
     }
-
 }
--- src/main/java/Model/Admin.java
+++ src/main/java/Model/Admin.java
@@ -1,13 +1,5 @@
 package Model;
 
 public class Admin extends UserAbstract {
-    public Admin(String[] adminAttributes) {
-        super();
-    }
-
-    @Override
-    public String toString() {
-        return "Admin has been created";
-    }
 }
 
