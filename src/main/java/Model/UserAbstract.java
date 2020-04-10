d0da1405a46e20a63a47d98f871650e92a89185f
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-04-10 12:59:58 +0200

    Add getters and setters to class UserAbstract

--- src/main/java/Model/UserAbstract.java
+++ src/main/java/Model/UserAbstract.java
@@ -6,4 +6,45 @@ public abstract class UserAbstract {
     protected String surname;
     protected String login;
     protected String password;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 }
