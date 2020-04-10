ccccf7a04071139b2e1d884e8601a62e2addd2f2
Dariusz Raba <rabadariusz@gmail.com>
2020-04-10 09:18:44 +0200

    Create Interface methods signatures

--- src/main/java/DAO/AdminDAO.java
+++ src/main/java/DAO/AdminDAO.java
@@ -1,4 +1,17 @@
 package DAO;
 
+import Model.Admin;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public interface AdminDAO {
+    public List<Admin> AdminList = new ArrayList<>();
+
+    public void getAllAdmins();   // return type need to be change for List<Admin>
+    public void addAdmin();
+    public void updateAdmin();
+    public void deleteAdmin();
+
+    public List<Admin> getAdminList();
 }
--- src/main/java/DAO/AdminDatabaseDAO.java
+++ src/main/java/DAO/AdminDatabaseDAO.java
@@ -1,4 +1,34 @@
 package DAO;
 
+import Model.Admin;
+
+import java.util.List;
+
 public class AdminDatabaseDAO implements AdminDAO {
+
+
+    @Override
+    public void getAllAdmins() {
+
+    }
+
+    @Override
+    public void addAdmin() {
+
+    }
+
+    @Override
+    public void updateAdmin() {
+
+    }
+
+    @Override
+    public void deleteAdmin() {
+
+    }
+
+    @Override
+    public List<Admin> getAdminList() {
+        return null;
+    }
 }
