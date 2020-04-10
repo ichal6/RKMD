b61d4d97974d97e4082a49b5a2681f42be04cc69
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-04-10 13:06:08 +0200

    Add Constructor to Client

--- src/main/java/Model/Client.java
+++ src/main/java/Model/Client.java
@@ -1,5 +1,17 @@
 package Model;
 
 
+import java.util.HashMap;
+
 public class Client extends UserAbstract {
+    HashMap<Product, Integer> basket;
+
+    public Client(String[] dataAboutClient) {
+        basket = new HashMap<>();
+        ID = dataAboutClient[0];
+        name = dataAboutClient[1];
+        surname = dataAboutClient[2];
+        login = dataAboutClient[3];
+        password = dataAboutClient[4];
+    }
 }
