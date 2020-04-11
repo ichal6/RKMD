56f4a52d871a4a2917f57719fc991111f57a01b3
Rafal Pajdak <rpajdak@gmail.com>
2020-04-11 17:51:33 +0200

    Write fake main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import Controller.ControllerShop;
+import Interaction.InputManager;
+import View.TerminalView;
 
 public class Main {
 
     public static void main(String[] args) {
+        ControllerShop shop =  new ControllerShop(new TerminalView(),new InputManager());
+        shop.run();
     }
 }
