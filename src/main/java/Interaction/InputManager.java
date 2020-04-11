3ac760766df6950a5930cf5450557f19896e4875
Rafal Pajdak <rpajdak@gmail.com>
2020-04-11 17:48:47 +0200

    Write initial skeleton.

--- src/main/java/Interaction/InputManager.java
+++ src/main/java/Interaction/InputManager.java
@@ -1,4 +1,11 @@
 package Interaction;
 
 public class InputManager {
+    public Integer getIntInput(String s) {
+        return 0;
+    }
+
+    public String getStringInput(String please_insert_your_login) {
+        return "string";
+    }
 }
--- src/main/java/View/AbstractView.java
+++ src/main/java/View/AbstractView.java
@@ -1,5 +1,23 @@
 package View;
 
+import Model.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class AbstractView {
 
+    public AbstractView(){
+
+    }
+    public void print(String[] menuContent, String label){
+        System.out.println(label);
+        System.out.println(menuContent);
+    }
+
+    public void print(HashMap<Product,Integer> basket) {
+        for(Map.Entry<Product,Integer> product : basket.entrySet()){
+            System.out.println(product.getKey().toString() + " " + product.getValue());
+        }
+    }
 }
