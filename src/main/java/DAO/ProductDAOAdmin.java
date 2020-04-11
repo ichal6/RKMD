3b0f6bf85fa554e22e84914387851d87abf7c9ec
Rafal Pajdak <rpajdak@gmail.com>
2020-04-11 11:47:12 +0200

    Change parameter in deleteProduct method.

--- src/main/java/DAO/ProductDAOAdmin.java
+++ src/main/java/DAO/ProductDAOAdmin.java
@@ -6,7 +6,7 @@ public interface ProductDAOAdmin extends ProductDAO {
 
     public void addProductToInventory(Product product, Integer quantity);
 
-    public void deleteProduct(Product product);
+    public void deleteProduct(Integer id);
 
     public void updateInventory(Product product, Integer quantity);
 }
