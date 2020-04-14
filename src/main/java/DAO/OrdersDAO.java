83f3569703bdb437b23986bfdabc6f8c5eba6413
Rafal Pajdak <rpajdak@gmail.com>
2020-04-14 20:59:38 +0200

    Some refactor in OrdersDao.

--- src/main/java/DAO/OrdersDAO.java
+++ src/main/java/DAO/OrdersDAO.java
@@ -1,14 +1,12 @@
 package DAO;
 
 import Model.Client;
-import Model.Order;
 import Model.Product;
 
 import java.util.HashMap;
-import java.util.List;
 
 public interface OrdersDAO {
-    public List getAllOrders();
-    public Order getSpecificOrder(Integer id);
+//    public List getAllOrders(); Do we really need this on this stage?
+//    public Order getSpecificOrder(Integer id); Do we really need this on this stage?
     public void createOrder(HashMap<Product, Integer> basket, Client client);
 }
