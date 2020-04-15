705c7da29cbe6eb45727a0cdc87d01b75c59e3df
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-04-15 12:58:24 +0200

    Add test files

---
+++ src/test/java/Model/ClientTest.java
@@ -0,0 +1,27 @@
+package Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClientTest {
+    String[] dataOfClient = {"ID", "name", "surname", "login", "password"};
+    String[] productData = {"name", "1234", "color", "typeOfFrame"};
+
+    @Test
+    public void should_add_two_count_of_simple_product_to_basket() {
+        //given
+        int quantitySimple = 2;
+        Client clientSimple = new Client(dataOfClient);
+        Product productSimple = new Product(productData);
+        //when
+        clientSimple.addToBasket(productSimple, quantitySimple);
+        int expected = clientSimple.getBasket().get(productSimple);
+        //then
+        assertEquals(quantitySimple, expected);
+    }
+
+    @Test
+    public void removeFromBasket() {
+    }
+}
