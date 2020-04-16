a8e0f9dd3f47cd0185050416458de1037fe3e5be
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-04-16 17:06:15 +0200

    Repair ClientTest
    Add fake class ClientDAOFake then implements skeleton methods and
    repair Clienttest, because methods for basket move to Controller
    Client class

---
+++ src/test/java/Model/ClientDAOFake.java
@@ -0,0 +1,48 @@
+package Model;
+
+import DAO.ClientsDAO;
+
+import java.util.List;
+
+import Model.Builder;
+
+public class ClientDAOFake implements ClientsDAO {
+
+    @Override
+    public void getAllClients() {
+
+    }
+
+    @Override
+    public void addClient(String[] clientToAdd) {
+
+    }
+
+    @Override
+    public void updateClient(Integer user_ID, String[] newAttributes) {
+
+    }
+
+    @Override
+    public void deleteClient(Integer user_ID) {
+
+    }
+
+    @Override
+    public boolean checkIsClient(String login, String password) {
+        return true;
+    }
+
+    @Override
+    public Client getClient(String login, String password) {
+        Builder builder = new Builder().withID("1").withName("Name").withSurname("Surname")
+                .withLogin("login").withPassword("Password");
+        Client client = new Client(builder);
+        return client;
+    }
+
+    @Override
+    public List<UserAbstract> getClientList() {
+        return null;
+    }
+}
